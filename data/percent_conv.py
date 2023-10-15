2988cd09cdf7624179274539e342cba8bd12dd1a
Mitch Cumming <mcredjames@gmail.com>
2023-10-15 16:15:40 +1100

    data updt

--- data/percent_conv.py
+++ data/percent_conv.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 # Load the CSV file into a DataFrame
-df = pd.read_csv("data/top_100_with_gender_students_rounded.csv")
+df = pd.read_csv("data/top_100_with_adjusted_coordinates_6 - Copy.csv")
 
 # Convert the "InternationalStudent" column to decimal format
 df["InternationalStudent"] = df["InternationalStudent"].str.rstrip('%').astype('float') / 100
 
 # Save the DataFrame back to a new CSV file
-df.to_csv("top_100_with_gender_students_decimal.csv", index=False)
+df.to_csv("top_100_with_gender_students_decimal_.csv", index=False)
